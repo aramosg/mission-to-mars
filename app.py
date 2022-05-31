529a6d73c8cdc7a8e596c6e5c51e66dc20b39d9d
Abraham Ramos <aramos@gmail.com>
2022-05-30 21:36:02 -0500

    Uploading Module 10.6.x

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flas, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
@@ -11,14 +11,16 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
+    #mars = mongo.db.mars.find_one_or_404()
     return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
     mars_data = scraping.scrape_all()
-    mars.update_once({}, {"$set": mars_data}, upsert=True)
+    #mars.update_once({}, {"$set": mars_data}, upsert=True)
+    mars.update_one({}, {"$set": mars_data}, upsert=True)
     return redirect('/', code=302)
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run()
